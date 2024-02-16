2cdcf547bf40876361724f4d13f7716bb9dfb1fe
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2024-02-17 01:01:28 +0530

    Lecture10

---
+++ Lecture10/index.js
@@ -0,0 +1,15 @@
+const root = Math.sqrt(144);
+
+console.log(root);
+
+const func1 = (a,b) => {
+    return math.abs(a-b);
+}
+
+const num = func1(5,3);
+
+console.log(num);
+
+const num2 = func1(3,5);
+
+console.log(num2);
